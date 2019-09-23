68bcb277d0dfa127fb353167a24fbed55a2e7829
AWhiteKnight <awhiteknight@unity-mail.de>
2019-09-23 10:25:54 +0200

    socket basics

--- main.js
+++ main.js
@@ -24,20 +24,23 @@ let bStopExecution = false;
 let ipAddress;
 let ipPort;
 let password;
+let client;
 
-function recieveLoop() {
+function recieveLoop(data) {
 	if(bStopExecution) {
 		return;
 	}
-	adapter.log.silly('recieveLoop');
-
+	adapter.log.debug('recieveLoop');
+	adapter.log.debug('DATA: ' + data);
 }
 
-function mainLoop() {
+async function mainLoop() {
 	if(bStopExecution) {
 		return;
 	}
-	adapter.log.silly('mainLoop');
+	await adapter.log.debug('mainLoop');
+	// Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+	client.write('I am Chuck Norris!');
 
 	timer = setTimeout(mainLoop, adapter.config.requestInterval*1000);
 }
@@ -81,9 +84,24 @@ class E3dc extends utils.Adapter {
 		this.setState('info.connection', false, true, async () => {
 			bStopExecution = false;
 			// establish socket connection
+			const net = require('net');
+
+			client = new net.Socket();
+			await client.connect(ipPort, ipAddress, async () => {
+				await adapter.log.debug('CONNECTED TO: ' + ipAddress + ':' + ipPort);
+			});
+			
+			// Add a 'data' event handler for the client socket
+			// data is what the server sent to this socket
+			client.on('data', recieveLoop);
+			
+			// Add a 'close' event handler for the client socket
+			client.on('close', () => {
+				adapter.log.debug('Connection closed');
+			});
 
 			// start mainLoop
-			mainLoop(); 
+			await mainLoop(); 
 			// set connection indicator
 			await this.setStateAsync('info.connection', true, true);
 		});
@@ -96,7 +114,10 @@ class E3dc extends utils.Adapter {
 	onUnload(callback) {
 		try {
 			bStopExecution = true;
-			if(timer) { clearTimeout(timer);}
+			if(timer) { clearTimeout(timer); }
+			// Close the client socket completely
+			adapter.log.debug('Closing connection');
+			if(client) { client.destroy(); }
 			this.log.info('cleaned everything up...');
 			callback();
 		} catch (e) {
